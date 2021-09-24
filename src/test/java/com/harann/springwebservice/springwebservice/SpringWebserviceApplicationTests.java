f0aa5515e3899bee276f4b3fede05dd23d4f8455
soo8129 <soo8129@gmail.com>
2021-09-24 11:18:25 +0900

    Initial commit

---
+++ src/test/java/com/harann/springwebservice/springwebservice/SpringWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harann.springwebservice.springwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
